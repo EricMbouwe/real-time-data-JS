b10d44e4f0ddbdcb4cc09f7a5c3de69f5424edf6
ericmbouwe <ericmbouwe@yahoo.fr>
2020-08-25 14:07:49 +0100

    added differents function to retrive data from icecat

--- src/index.js
+++ src/index.js
@@ -1,4 +1,54 @@
 import './main.scss'
 
-console.log("the prod mode");
-console.log('this is a live test and in real time');
+console.log("Icecat live test dev ")
+
+// make the Live Icecat library call
+setTimeout(function () {
+  IcecatLive.getDatasheet('#IcecatLive', {
+    'UserName': 'openIcecat-live', 
+    'Brand': 'HP', 
+    'ProductCode': 'F0Y97EA'
+  }, 'en');
+}, 200)
+
+function getDataByAllParams() {
+  setTimeout(function () {
+    IcecatLive.getDatasheet('#IcecatLive', {
+      'UserName': 'userShopName',
+      'GTIN': '1234567891234',
+      'Brand': 'abcdefg',
+      'ProductCode': '123abcdefg',
+      'IcecatProductId': '123456',
+    }, 'en')
+  }, 200)
+}
+
+// call by Brand + Manufacturer Product Code
+
+function getDataByBrand() {
+  IcecatLive.getDatasheet('#IcecatLive', {
+    'UserName': 'demoshop',
+    'Brand': 'Philips', 
+    'ProductCode': '123456789'
+  }, 'en', 200)
+}
+
+
+// call by GTIN code (EAN or UPC or JAN)
+
+function getDataByGTIN() {
+  IcecatLive.getDatasheet('#IcecatLive', {
+    'UserName': 'demoshop',
+    'GTIN': '8478097595798729'
+  }, 'en', 200)
+}
+
+
+// call by IcecatProductId
+
+function getDataByProductID() {
+  IcecatLive.getDatasheet('#IcecatLive', {
+    'UserName': 'demoshop',
+    'IcecatProductId': '123456'
+  }, 'en', 200);
+}
